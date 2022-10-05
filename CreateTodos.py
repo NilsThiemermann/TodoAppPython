8f8c5cf33db3e972f69253be41b1763268e8aa50
NilsThiemermann <nils.thiemermann@stud.edubs.ch>
2022-10-05 18:20:20 +0200

    Select from DB & Creating new Data are working

--- CreateTodos.py
+++ CreateTodos.py
@@ -1,7 +1,41 @@
 from tkinter import *
 import sqlite3
 
+conn = sqlite3.connect('todo.db')
+
 class Create:
     
     def __init__(self, master):
-        return
+
+        titles = ["Todo:", "Zeitaufwand:", "Wichtigkeit:",  "Wird Gemacht bis:"]
+
+        for i in range(len(titles)):
+            self.l = Label(master, text=titles[i], fg='black', font=('Arial', 14))
+            self.l.grid(row=i, column=1)
+
+        self.e1 = Entry(master, fg='black', width=40, font=('Arial', 14))
+        self.e1.grid(row=0, column=2)
+        self.e2 = Entry(master, fg='black', width=40, font=('Arial', 14))
+        self.e2.grid(row=1, column=2)
+        self.e3 = Entry(master, fg='black', width=40, font=('Arial', 14))
+        self.e3.grid(row=2, column=2)
+        self.e4 = Entry(master, fg='black', width=40, font=('Arial', 14))
+        self.e4.grid(row=3, column=2)
+
+        self.sendBtn = Button(master, text='Send', command=self.insertDB)
+        self.sendBtn.grid(row=5, column=2)
+        
+    def insertDB(self):
+        cur = conn.cursor()
+
+        v1 = self.e1.get()
+        v2 = self.e2.get()
+        v3 = self.e3.get()
+        v4 = self.e4.get()
+
+        values = [v1, v2, v3, v4]
+
+        sql = """INSERT INTO tbl_Todo(todo, expenditure_of_time, fk_importance, expiry_date) VALUES(?,?,?,?)"""
+
+        cur.execute(sql, values)
+        conn.commit()
--- SelectTodos.py
+++ SelectTodos.py
@@ -1,9 +1,12 @@
 from tkinter import *
 import sqlite3
+from tkinter.font import BOLD
 
 import app
 
 conn = sqlite3.connect('todo.db')
+
+widgets = []
 
 class Select:
 
@@ -17,18 +20,22 @@ class Select:
         total_rows = len(selected)
         total_columns = len(selected[0])
 
-        print(selected)
-
+        titles = ["Todo:", "Zeitaufwand:", "Wichtigkeit:", "Erstell Datum:", "Wird Gemacht bis:"]
+        
         for i in range(total_rows):
             for j in range(total_columns):
-                
-                self.e = Entry(master, width=20, fg='black', font=('Arial', 16, 'bold'))
+                self.la = Label(master, text=titles[j], width=18, fg='black', font=('Arial', 10, UNDERLINE))
+                self.la.grid(row=1, column=j)
+                self.l = Label(master, text=selected[i][j], width=18, fg='black', font=('Arial', 10))
+                self.l.grid(row=i+2, column=j)
+                widgets.append(self.la)
+                widgets.append(self.l)
 
-                self.e.grid(row=i, column=j)
-                self.e.insert(END, selected[i][j])
+        self.btnCreate = Button(master, text="Create Todo", command=self.createTodos)
+        self.btnCreate.grid(row=total_rows+3, column=total_columns)
+        widgets.append(self.btnCreate)
 
-    def createTodos(self):
+    def createTodos():
         #remove the widgets from the frame
-        #app.forget()
-
+        app.forget(widgets)
         return app.createTodos()
